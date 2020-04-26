49d42928c69ea9fd17fe583fb797d9ece8ae83db
MHenrie <silvamarlon87@gmail.com>
2020-04-26 19:14:21 -0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const routes = require('./routes');
+
+const app = express();
+
+const HOST = '0.0.0.0';
+const PORT = 3333;
+
+app.use(express.json());
+app.use(routes);
+
+app.listen(PORT, HOST, () => console.log('Application Running...'));
