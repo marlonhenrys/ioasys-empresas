8940c5f79ef117a4159608ae1e03070a8a96ef53
MHenrie <silvamarlon87@gmail.com>
2020-05-21 10:11:11 -0300

    update enterprise

--- src/app/services/enterprise/index.js
+++ src/app/services/enterprise/index.js
@@ -1,13 +1,15 @@
-const create = require('./create')
 const validation = require('./validation')
+const create = require('./create')
 const findAll = require('./findAll')
 const findOne = require('./findOne')
+const update = require('./update')
 const destroy = require('./destroy')
 
 module.exports = {
-  create,
   validation,
+  create,
   findAll,
   findOne,
+  update,
   destroy
 }
--- src/app/services/enterprise/validation.js
+++ src/app/services/enterprise/validation.js
@@ -1,4 +1,5 @@
 const ApplicationError = require('../../utils/errorHandler')
+const enterpriseRepository = require('../../repositories/EnterpriseRepository')
 const { ADM, MGR } = require('../../utils/typeUsers')
 
 module.exports = {
@@ -21,6 +22,30 @@ module.exports = {
 
       if (!permission) {
         throw new ApplicationError('Você não tem permissão para realizar esta ação', 403)
+      }
+    } catch (error) {
+      console.error(error)
+      throw error
+    }
+  },
+
+  update: async (data, auth) => {
+    try {
+      let permission = false
+      const enterprise = await enterpriseRepository.findById(data.id)
+
+      if (!enterprise) {
+        throw new ApplicationError('Empresa não encontrada', 404)
+      }
+
+      if (auth.type === ADM) {
+        permission = true
+      } else if (auth.type === MGR) {
+        permission = enterprise.manager_id === auth.id
+      }
+
+      if (!permission) {
+        throw new ApplicationError('Você não tem permissão para editar este registro', 403)
       }
     } catch (error) {
       console.error(error)
